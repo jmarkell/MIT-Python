f01ad5dd5ab0bde72546d07dcb98989072f835aa
jmarkell <jmarkell@all-saints.org>
2016-05-20 14:15:25 -0500

    Finished MIT Problem Set 1

---
+++ ps1c.py
@@ -0,0 +1,42 @@
+# Problem Set 1
+# Name: Markell
+# Collaborators: N/A
+# Time Spent: 
+# Take an outstanding balance and annual interest rate and calculate the minimum
+# monthly payment to pay off the balance within one year using bisection method. 
+
+from __future__ import print_function
+
+outstanding = float(raw_input("Enter the outstanding balance on your credit card: "))
+annual_rate = float(raw_input("Enter the annual credit card interest rate as a decimal: "))
+
+# interest = outstanding * annual_rate / 12
+# principal = min_payment - interest 
+# balance = outstanding - principal
+# outstanding = balance
+
+lower_bound = outstanding / 12.0
+upper_bound = (outstanding * (1 + (annual_rate / 12.0)) ** 12.0) / 12.0
+min_payment = lower_bound
+
+flag = True
+
+while flag:
+	temp_outstanding = outstanding
+	for month in xrange(1,13):
+		interest = temp_outstanding * annual_rate / 12.0
+		principal = min_payment - interest
+		balance = temp_outstanding - principal
+		
+		if balance <= 0:
+			print("\nRESULT")
+			print("Monthly payment to pay off debt in 1 year: %0.2f" % min_payment)
+			print("Number of months needed: %d" % month)
+			print("Balance: %0.2f" % balance)
+			flag = False
+			break
+		
+		temp_outstanding = balance
+	
+	min_payment += .01
+	
